99d8474f5a907ac4dee3f94311679ef8160459a9
droid1700 <60330760+droid1700@users.noreply.github.com>
2024-02-25 19:31:08 -0500

    Working Prototype

---
+++ EUIV-SaveScummer/EUIV-SaveScummer/FileOps.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EUIV_SaveScummer
+{
+	internal class FileOps
+	{
+		private byte[] savedHash;
+		private string savedGameFilePath;
+		private string gameFileName;
+		private string dirToSaveScumTo;
+
+		public FileOps(string filePath)
+		{
+			SetFileHash(filePath);
+			savedGameFilePath = filePath;
+			gameFileName = filePath.Split('\\')[filePath.Split('\\').Length - 1].Substring(0, filePath.Split('\\')[filePath.Split('\\').Length - 1].Length - 4);
+		}
+
+		/// <summary>
+		/// Sets the savedHash based on the passed filepath
+		/// </summary>
+		/// <param name="filepath"></param>
+		private void SetFileHash(string filepath)
+		{
+			try
+			{
+				HashAlgorithm sha1 = HashAlgorithm.Create();
+				using(FileStream stream = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+				{
+					savedHash = sha1.ComputeHash(stream);
+				}
+			}
+			catch { }
+		}
+
+		/// <summary>
+		/// Check if the file state has changed
+		/// </summary>
+		/// <returns></returns>
+		public bool FileChanged()
+		{
+			if(String.IsNullOrEmpty(savedGameFilePath))
+			{
+				return false; //Game file doesn't exist
+			}
+
+			byte[] hashCheck;
+			HashAlgorithm sha1 = HashAlgorithm.Create();
+
+			using(FileStream stream = new FileStream(savedGameFilePath, FileMode.Open, FileAccess.Read))
+			{
+				hashCheck = sha1.ComputeHash(stream);
+			}
+
+			bool returnValue = !hashCheck.SequenceEqual(savedHash);
+
+			savedHash = hashCheck;
+
+			return returnValue;
+		}
+
+		/// <summary>
+		/// Set the save scum directory
+		/// </summary>
+		/// <param name="dirPath"></param>
+		public void SetSaveDir(string dirPath)
+		{
+			dirToSaveScumTo = dirPath;
+		}
+
+		/// <summary>
+		/// Save Scum
+		/// </summary>
+		public void SaveScum()
+		{
+			if(String.IsNullOrEmpty(savedGameFilePath) || String.IsNullOrEmpty(dirToSaveScumTo))
+			{
+				return;
+			}
+
+			DateTime time = DateTime.Now;
+			string path = dirToSaveScumTo + "\\" + gameFileName + "_" + time.Year + time.Month.ToString().PadLeft(2, '0') + time.Day.ToString().PadLeft(2, '0') + time.Hour.ToString().PadLeft(2, '0') + time.Minute.ToString().PadLeft(2, '0') + ".eu4";
+
+			File.Copy(savedGameFilePath, path);
+		}
+
+	}
+}
---
+++ EUIV-SaveScummer/EUIV-SaveScummer/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EUIV_SaveScummer
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private FileOps fileops;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Load button click
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void LoadButton_Click(object sender, RoutedEventArgs e)
+		{
+			Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+			dlg.DefaultExt = ".eu4";
+			dlg.Filter = "EU4 Game Files (.eu4)|*.eu4";
+
+			//Build the default path
+			string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+			path += "\\Paradox Interactive\\Europa Universalis IV\\save games";
+
+			dlg.InitialDirectory = path;
+
+			if(dlg.ShowDialog().Value)
+			{
+				//Create the task that will continue to run
+				Task.Factory.StartNew(() =>
+				{
+					fileops = new FileOps(dlg.FileName);
+
+					while(true)
+					{
+						if(fileops.FileChanged())
+						{
+							//Save a copy of the file
+							fileops.SaveScum();
+						}
+
+						//Check again in 5 minutes
+						Thread.Sleep(300000);
+					}
+				}, CancellationToken.None,
+				TaskCreationOptions.LongRunning,
+				TaskScheduler.Default
+				);
+			}
+		}
+
+		/// <summary>
+		/// Set where to save copies of the save game
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void DirSetButton_Click(object sender, RoutedEventArgs e)
+		{
+			//https://stackoverflow.com/questions/11624298/how-do-i-use-openfiledialog-to-select-a-folder
+
+			if(fileops == null)
+			{
+				return;
+			}
+
+			System.Windows.Forms.FolderBrowserDialog fdb = new System.Windows.Forms.FolderBrowserDialog();
+			System.Windows.Forms.DialogResult result = fdb.ShowDialog();
+
+			if(result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrEmpty(fdb.SelectedPath))
+			{
+				fileops.SetSaveDir(fdb.SelectedPath);
+			}
+		}
+	}
+}
